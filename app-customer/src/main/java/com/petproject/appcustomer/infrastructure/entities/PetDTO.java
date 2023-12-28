b4972c38938d7dd6319796513502a8e17d3a3406
andres.mera <feli041089@gmail.com>
2023-12-28 09:03:27 -0500

    dev-apcus : Creacion del dto de pet.

---
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/entities/PetDTO.java
@@ -0,0 +1,29 @@
+package com.petproject.appcustomer.infrastructure.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetDTO {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String birthdate;
+    private String age;
+    private String gender;
+    private String petCategory;
+    private String race;
+    private String weight;
+    private String characteristicsExtremities;
+
+    private Integer idCustomerEntity;
+    private Integer idHairColorEntity;
+    private Integer idBodySizeEntity;
+    private Integer idTypeFigureEntity;
+    private Integer idEyeColorEntity;
+}
