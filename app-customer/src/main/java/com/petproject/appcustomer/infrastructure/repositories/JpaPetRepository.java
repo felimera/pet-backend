45611db935329fa74d8a3da177f061bb46643430
andres.mera <feli041089@gmail.com>
2023-12-28 10:51:44 -0500

    dev-apcus : Creacion del repositorio y adapter de pet.

---
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/repositories/JpaPetRepository.java
@@ -0,0 +1,9 @@
+package com.petproject.appcustomer.infrastructure.repositories;
+
+import com.petproject.appcustomer.domain.models.in.PetEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaPetRepository extends JpaRepository<PetEntity,Integer> {
+}
---
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/repositories/JpaPetRepositoryAdapter.java
@@ -0,0 +1,78 @@
+package com.petproject.appcustomer.infrastructure.repositories;
+
+import com.petproject.appcustomer.domain.models.in.PetEntity;
+import com.petproject.appcustomer.domain.ports.out.PetRepositoryPort;
+import com.petproject.appcustomer.infrastructure.entities.PetDTO;
+import com.petproject.appcustomer.infrastructure.mapper.PetMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class JpaPetRepositoryAdapter implements PetRepositoryPort {
+
+    private final JpaPetRepository jpaPetRepository;
+
+    private final JpaColorEntityRepository jpaColorEntityRepository;
+    private final JpaBodySizeEntityRepository jpaBodySizeEntityRepository;
+    private final JpaTypeFigureEntityRepository jpaTypeFigureEntityRepository;
+
+    @Override
+    public PetDTO save(PetDTO dto) {
+        PetEntity newPetEntity = PetMapper.INSTANCE.fromDomainModel(dto);
+
+        jpaColorEntityRepository.findById(dto.getIdHairColorEntity()).ifPresent(newPetEntity::setHairColorEntity);
+        jpaBodySizeEntityRepository.findById(dto.getIdBodySizeEntity()).ifPresent(newPetEntity::setBodySizeEntity);
+        jpaTypeFigureEntityRepository.findById(dto.getIdTypeFigureEntity()).ifPresent(newPetEntity::setTypeFigureEntity);
+        jpaColorEntityRepository.findById(dto.getIdEyeColorEntity()).ifPresent(newPetEntity::setEyeColorEntity);
+
+        PetEntity savedPetEntity = jpaPetRepository.save(newPetEntity);
+        return PetMapper.INSTANCE.toDomainModel(savedPetEntity);
+    }
+
+    @Override
+    public Optional<PetDTO> findById(Integer id) {
+        return jpaPetRepository.findById(id).map(PetMapper.INSTANCE::toDomainModel);
+    }
+
+    @Override
+    public List<PetDTO> findAll() {
+        return jpaPetRepository.findAll().stream().map(PetMapper.INSTANCE::toDomainModel).toList();
+    }
+
+    @Override
+    public PetDTO update(Integer id, PetDTO dto) {
+        Optional<PetEntity> optionalPetEntity = jpaPetRepository.findById(id);
+        if (optionalPetEntity.isPresent()) {
+            optionalPetEntity.get().setName(dto.getName());
+            optionalPetEntity.get().setPhone(dto.getPhone());
+            optionalPetEntity.get().setBirthdate(LocalDate.parse(dto.getBirthdate()));
+            optionalPetEntity.get().setAge(dto.getAge());
+            optionalPetEntity.get().setGender(dto.getGender());
+            optionalPetEntity.get().setPetCategory(dto.getPetCategory());
+            optionalPetEntity.get().setRace(dto.getRace());
+            optionalPetEntity.get().setWeight(dto.getWeight());
+            optionalPetEntity.get().setCharacteristicsExtremities(dto.getCharacteristicsExtremities());
+
+            jpaColorEntityRepository.findById(dto.getIdHairColorEntity()).ifPresent(colorEntity -> optionalPetEntity.get().setHairColorEntity(colorEntity));
+            jpaBodySizeEntityRepository.findById(dto.getIdBodySizeEntity()).ifPresent(bodySizeEntity -> optionalPetEntity.get().setBodySizeEntity(bodySizeEntity));
+            jpaTypeFigureEntityRepository.findById(dto.getIdTypeFigureEntity()).ifPresent(typeFigureEntity -> optionalPetEntity.get().setTypeFigureEntity(typeFigureEntity));
+            jpaColorEntityRepository.findById(dto.getIdEyeColorEntity()).ifPresent(colorEntity -> optionalPetEntity.get().setEyeColorEntity(colorEntity));
+
+            PetEntity updatePetEntity = jpaPetRepository.save(optionalPetEntity.get());
+            return PetMapper.INSTANCE.toDomainModel(updatePetEntity);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Integer id) {
+        Optional<PetEntity> optionalPetEntity = jpaPetRepository.findById(id);
+        optionalPetEntity.ifPresent(jpaPetRepository::delete);
+        return !jpaPetRepository.existsById(id);
+    }
+}
