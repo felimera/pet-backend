51eaef8fed4f85dfe1092fa266046b4b797cc413
andres.mera <feli041089@gmail.com>
2024-01-02 15:31:50 -0500

    dev-apgat : Creacion del microservice.

---
+++ app-gateway/src/test/java/com/proyect/appgateway/AppGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.proyect.appgateway;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+        AppGatewayApplication myClass = new AppGatewayApplication();
+        Assertions.assertThat(myClass).isNotNull();
+    }
+
+}
