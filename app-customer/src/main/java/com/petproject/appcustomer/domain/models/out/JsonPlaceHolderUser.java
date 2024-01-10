353731ff3f0a9a63eae8dcfebe703def04ebd082
andres.mera <feli041089@gmail.com>
2024-01-10 15:03:48 -0500

    dev-apcus : Modificando el typo de datos del atributo de respuesta.

--- app-customer/src/main/java/com/petproject/appcustomer/domain/models/out/JsonPlaceHolderUser.java
+++ app-customer/src/main/java/com/petproject/appcustomer/domain/models/out/JsonPlaceHolderUser.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class JsonPlaceHolderUser {
-    private Long id;
+    private Integer id;
     private String name;
     private String email;
 }
