4dce1765d14938d25f703a36161fe680f4ee1946
andres.mera <feli041089@gmail.com>
2024-01-10 14:42:42 -0500

    dev-apcus : Creacion del modelo User de consumo extreno al api.

---
+++ app-customer/src/main/java/com/petproject/appcustomer/domain/models/out/JsonPlaceHolderUser.java
@@ -0,0 +1,12 @@
+package com.petproject.appcustomer.domain.models.out;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JsonPlaceHolderUser {
+    private Long id;
+    private String name;
+    private String email;
+}
