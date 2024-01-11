39d09315cec24aae047413733565762af2c0bca5
andres.mera <feli041089@gmail.com>
2024-01-11 15:18:02 -0500

    dev-apcus : Agregando el CrossOrigin para la comunicacion con el front.

--- app-customer/src/main/java/com/petproject/appcustomer/infrastructure/controllers/CustomerController.java
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/controllers/CustomerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping(path = "api/customer")
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class CustomerController {
 
