fcf2117560ef97964dfef4e0ed105ecc2d7f94bf
andres.mera <feli041089@gmail.com>
2024-01-15 09:32:36 -0500

    dev-apcus : Modificacion del mapeo por los cambios de los atributos.

--- app-customer/src/main/java/com/petproject/appcustomer/infrastructure/mapper/CustomerMapper.java
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/mapper/CustomerMapper.java
@@ -2,8 +2,10 @@ package com.petproject.appcustomer.infrastructure.mapper;
 
 import com.petproject.appcustomer.domain.models.in.CustomerEntity;
 import com.petproject.appcustomer.infrastructure.entities.CustomerDTO;
+import com.petproject.appcustomer.infrastructure.util.CadenaUtil;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,8 +13,13 @@ public interface CustomerMapper {
 
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
+    @Mapping(target = "isOlder", expression = "java(isOlderConvert(dto))")
     CustomerEntity fromDomainModel(CustomerDTO dto);
 
     @InheritInverseConfiguration
     CustomerDTO toDomainModel(CustomerEntity entity);
+
+    default String isOlderConvert(CustomerDTO dto) {
+        return CadenaUtil.convertTrueFalse(dto.getIsOlder());
+    }
 }
