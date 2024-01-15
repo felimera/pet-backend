a107ed4a275b42c702294a9340f92ff64e252bfb
andres.mera <feli041089@gmail.com>
2024-01-15 09:35:56 -0500

    dev-apgat : Modificacndo del mappeo.

--- app-gateway/src/main/java/com/proyect/appgateway/controller/mapper/UserMapper.java
+++ app-gateway/src/main/java/com/proyect/appgateway/controller/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.proyect.appgateway.controller.mapper;
 import com.proyect.appgateway.controller.dto.UserDto;
 import com.proyect.appgateway.controller.dto.auth.SignUpDto;
 import com.proyect.appgateway.models.User;
-import com.proyect.appgateway.util.Constants;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -19,10 +18,5 @@ public interface UserMapper {
     @InheritInverseConfiguration
     User toEntity(UserDto userDto);
 
-    @Mapping(target = "name", expression = "java(getNombres(signUpDto))")
     User toSignUp(SignUpDto signUpDto);
-
-    default String getNombres(SignUpDto signUpDto) {
-        return signUpDto.getNombres().replace(Constants.AMPERSAND, Constants.SPACE);
-    }
 }
