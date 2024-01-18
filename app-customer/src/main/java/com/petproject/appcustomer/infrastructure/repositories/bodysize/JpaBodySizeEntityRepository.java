63f131905bd29262d6ed64be0846a62813f91b21
andres.mera <feli041089@gmail.com>
2024-01-18 08:59:39 -0500

    dev-apcus : Moviendo el repository a otro pack.

--- app-customer/src/main/java/com/petproject/appcustomer/infrastructure/repositories/bodysize/JpaBodySizeEntityRepository.java
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/repositories/bodysize/JpaBodySizeEntityRepository.java
@@ -1,9 +1,14 @@
-package com.petproject.appcustomer.infrastructure.repositories;
+package com.petproject.appcustomer.infrastructure.repositories.bodysize;
 
 import com.petproject.appcustomer.domain.models.in.BodySizeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JpaBodySizeEntityRepository extends JpaRepository<BodySizeEntity, Integer> {
+    @Query("select bs from BodySizeEntity bs  where bs.active = :active")
+    List<BodySizeEntity> getAllBodySize(Boolean active);
 }
