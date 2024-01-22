1d3652083d8674fbc658596aead915190b0db771
andres.mera <feli041089@gmail.com>
2024-01-22 14:46:03 -0500

    dev-media : Creacion del modelo PhotoPet.java

---
+++ app-media/src/main/java/com/project/appmedia/models/PhotoPet.java
@@ -0,0 +1,34 @@
+package com.project.appmedia.models;
+
+import com.project.appmedia.util.config.BooleanConverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_photo_pet")
+public class PhotoPet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pp_id")
+    private Integer id;
+    @Column(name = "pp_name")
+    private String name;
+    @Column(name = "pp_extension")
+    private String extension;
+    @Column(name = "pp_location")
+    private String location;
+    @Column(name = "pp_profile_picture")
+    @Convert(converter = BooleanConverter.class)
+    private Boolean profilePicture;
+    @Column(name = "pp_comment")
+    private String comment;
+    @Column(name = "pp_pet_id")
+    private Integer petId;
+}
