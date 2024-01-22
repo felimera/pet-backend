9a60057c3ad4747225b5764046dd538cb6abeabe
andres.mera <feli041089@gmail.com>
2024-01-22 08:42:29 -0500

    dev-media : Creacion del micro servicio media.

---
+++ app-media/src/main/java/com/project/appmedia/AppMediaApplication.java
@@ -0,0 +1,13 @@
+package com.project.appmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMediaApplication.class, args);
+	}
+
+}
---
+++ app-media/src/test/java/com/project/appmedia/AppMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
