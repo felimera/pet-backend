28bf10d16d49dbebf2282d12ecf512eeb4d67d3b
andres.mera <feli041089@gmail.com>
2024-01-22 09:27:49 -0500

    dev-media : Creacion y desarrollo el controlador del storage.

---
+++ app-media/src/main/java/com/project/appmedia/controller/MediaController.java
@@ -0,0 +1,47 @@
+package com.project.appmedia.controller;
+
+import com.project.appmedia.service.StorageService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "api/media")
+@AllArgsConstructor
+public class MediaController {
+
+    private final StorageService storageService;
+    private final HttpServletRequest request;
+
+    @PostMapping(path = "/upload")
+    public Map<String, String> uploadFile(@RequestParam(name = "file") MultipartFile multipartFile) {
+        String path = storageService.store(multipartFile);
+        String host = request.getRequestURI().toString().replace(request.getRequestURI(), "");
+        String url = ServletUriComponentsBuilder
+                .fromHttpUrl(host)
+                .path("/media/")
+                .path(path)
+                .toString();
+
+        return Map.of("url", url);
+    }
+
+    @GetMapping(path = "{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) throws IOException {
+        Resource file = storageService.loadAsResource(filename);
+        String contentType = Files.probeContentType(file.getFile().toPath());
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, contentType)
+                .body(file);
+    }
+}
