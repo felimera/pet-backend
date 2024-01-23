d1f231932c2cfcac4873e7f832e0d9b73d225c63
andres.mera <feli041089@gmail.com>
2024-01-23 08:27:24 -0500

    dev-media : Nuevos atributos de dto.

--- app-media/src/main/java/com/project/appmedia/controller/dto/PhotoPetDTO.java
+++ app-media/src/main/java/com/project/appmedia/controller/dto/PhotoPetDTO.java
@@ -1,6 +1,8 @@
 package com.project.appmedia.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,8 +10,16 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PhotoPetDTO {
     private Integer id;
+    private String name;
+    private String extension;
+    private String location;
+    private Boolean profilePicture;
+    private String comment;
+    private Integer petId;
+
     private List<MultipartFile> multipartFileList;
-    private Integer idPet;
 }
