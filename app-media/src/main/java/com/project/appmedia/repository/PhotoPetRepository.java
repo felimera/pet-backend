c24e86424f7ba2f48590e8040f4c3c9225236a0f
andres.mera <feli041089@gmail.com>
2024-01-23 08:26:21 -0500

    dev-media : Nueva consulta Para PhotoPet.

--- app-media/src/main/java/com/project/appmedia/repository/PhotoPetRepository.java
+++ app-media/src/main/java/com/project/appmedia/repository/PhotoPetRepository.java
@@ -4,8 +4,12 @@ import com.project.appmedia.models.PhotoPet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhotoPetRepository extends JpaRepository<PhotoPet, Integer> {
 
     boolean existsByName(String name);
+
+    List<PhotoPet> findAllByPetId(Integer petId);
 }
