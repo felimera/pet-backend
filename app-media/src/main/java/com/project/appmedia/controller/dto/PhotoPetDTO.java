526438ae83f91b0341d66a7cad5788c54840e623
andres.mera <feli041089@gmail.com>
2024-01-23 15:02:30 -0500

    dev-media : Agregando todas las validaciones necesaria para el dto.

--- app-media/src/main/java/com/project/appmedia/controller/dto/PhotoPetDTO.java
+++ app-media/src/main/java/com/project/appmedia/controller/dto/PhotoPetDTO.java
@@ -1,12 +1,12 @@
 package com.project.appmedia.controller.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -14,12 +14,24 @@ import java.util.List;
 @NoArgsConstructor
 public class PhotoPetDTO {
     private Integer id;
+    @NotEmpty(message = "The photo name cannot be empty.")
+    @NotNull(message = "The photo name cannot be null.")
+    @Size(max = 50, message = "The photo name has a maximum of 50 characters.")
     private String name;
+    @NotEmpty(message = "The photo extension cannot be empty.")
+    @NotNull(message = "The photo extension cannot be null.")
+    @Size(max = 5, message = "The photo extension has a maximum of 5 characters.")
     private String extension;
+    @NotEmpty(message = "The photo location cannot be empty.")
+    @NotNull(message = "The photo location cannot be null.")
+    @Size(max = 500, message = "The photo location has a maximum of 500 characters.")
     private String location;
+    @NotEmpty(message = "The photo profilePicture cannot be empty.")
+    @NotNull(message = "The photo profilePicture cannot be null.")
     private Boolean profilePicture;
+    @Size(max = 1000, message = "The photo comment has a maximum of 1000 characters.")
     private String comment;
+    @NotEmpty(message = "The photo petId cannot be empty.")
+    @NotNull(message = "The photo petId cannot be null.")
     private Integer petId;
-
-    private List<MultipartFile> multipartFileList;
 }
