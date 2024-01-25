1a0e1ce2e1cb062cb1ba118302689cc13f90b0e6
andres.mera <feli041089@gmail.com>
2024-01-25 14:07:29 -0500

    dev-apcus : Nuevo constante.

--- app-customer/src/main/java/com/petproject/appcustomer/infrastructure/util/Constants.java
+++ app-customer/src/main/java/com/petproject/appcustomer/infrastructure/util/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
 
     public static final String F = "F";
     public static final String T = "T";
+
+    public static final String PERCENTAGE="%";
     public static final String REGULARPHRASE_BIRTHDATE = "^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$";
     public static final String MESSAGE_NOT_FOUND = "No results were found.";
 }
